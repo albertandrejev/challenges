4158a2ae370be4885b2d0109e6fc4d3a9710d4c0
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-10 01:39:25 +0300

    Connect redux store to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
+import { Provider } from "react-redux";
 
+import { configureStore } from "./store";
 import theme from "./utils/theme";
+
+const store = configureStore();
 
 function App(): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
+    </Provider>
   );
 }
 
