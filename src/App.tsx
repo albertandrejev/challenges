2bb9f488177b63d0a8f53872c1561d78409399bb
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-09 22:19:11 +0300

    Adapt inital components to work with MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,14 @@
-import "./App.css";
-
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 
-import logo from "./logo.svg";
+import theme from "./utils/theme";
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
 }
 
