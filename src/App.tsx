9ac40419e2430abff7b587dc9d8ef57de0728c54
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-10 13:20:45 +0300

    Add app stub

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
-import { Provider } from "react-redux";
-
-import { configureStore } from "./store";
-import theme from "./utils/theme";
-
-const store = configureStore();
 
 function App(): JSX.Element {
-  return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
-    </Provider>
-  );
+  return <React.Fragment />;
 }
 
 export default App;
