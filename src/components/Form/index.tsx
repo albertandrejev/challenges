67aa097f99177db179b2a11f7988f834c49fc6ff
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-11 00:01:42 +0300

    Add forms support

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export interface FormValues {
+  [key: string]: string;
+}
+
+export interface ValidationError {
+  [key: string]: string;
+}
+
+interface Props {
+  readonly onSubmit: (
+    event?: React.SyntheticEvent<HTMLFormElement>,
+  ) => Promise<object | undefined> | undefined;
+  readonly children: React.ReactNode;
+  readonly className?: string;
+}
+
+const Form: React.FunctionComponent<Props> = ({ onSubmit, className, children }): JSX.Element => (
+  <form className={className} onSubmit={onSubmit}>
+    {children}
+  </form>
+);
+
+export default Form;
