7dd64de1df547ef34f98b04db54772decf94f2c0
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-10 12:44:04 +0300

    Refactor startup components

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,34 @@
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { configureStore } from "./store";
+import theme from "./utils/theme";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+const store = configureStore();
+
+const render = (Component: React.ComponentType): void => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component />
+        </ThemeProvider>
+      </Provider>
+    </React.StrictMode>,
+    document.getElementById("root"),
+  );
+};
+
+render(App);
+
+if (module.hot) {
+  module.hot.accept("./routes", (): void => {
+    const NextApp = require("./routes").default;
+    render(NextApp);
+  });
+}
