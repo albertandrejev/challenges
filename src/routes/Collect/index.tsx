20db30f3693ea6327a8492b61e52e9f0b04e5792
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-10 14:12:08 +0300

    Add MUI components exports

---
+++ src/routes/Collect/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import Layout from "./components/Layout";
+
+const Collect: React.FunctionComponent = (): JSX.Element => {
+  return <Layout />;
+};
+
+export default Collect;
