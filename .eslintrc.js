62bd1c3a986d59b1e24c0aff95586f0be6e955de
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-11 17:32:13 +0300

    Add ban-ts-ignore rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
         allowExpressions: true,
       },
     ],
+    "@typescript-eslint/ban-ts-ignore": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-empty-interface": "off",
     "@typescript-eslint/no-explicit-any": "off",
