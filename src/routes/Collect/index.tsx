04903fda90fd0e437a460e7d01c291672cae5bae
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-12 00:16:37 +0300

    Submit data to REST endpoint

--- src/routes/Collect/index.tsx
+++ src/routes/Collect/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 import { FormValues } from "../../components/Form";
-import Layout, { ConsentValue } from "./components/Layout";
+import { Consent } from "../../store/consents";
+import Layout, { ConsentValue, EMAIL_INPUT_FIELD, NAME_INPUT_FIELD } from "./components/Layout";
 
 const consents: ConsentValue[] = [
   { name: "news", label: "Receive newsletter" },
@@ -10,7 +11,26 @@ const consents: ConsentValue[] = [
 ];
 
 const Collect: React.FunctionComponent = (): JSX.Element => {
-  const onSubmit = (values: FormValues): void => {
+  const onSubmit = async (values: FormValues): Promise<void> => {
+    // Assembling data object with user selected consents
+    const data: Consent = {
+      name: values[NAME_INPUT_FIELD],
+      email: values[EMAIL_INPUT_FIELD],
+      items: Object.entries(values)
+        .filter(([key]) => consents.find((consent) => consent.name === key))
+        .map(([key, value]) => ({
+          title: consents.find((consent) => consent.name === key)!.label, // eslint-disable-line @typescript-eslint/no-non-null-assertion
+          agree: Boolean(value),
+        })),
+    };
+
+    await fetch("/consents", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
     console.log("Submitted values", values);
   };
 
