b7bdd12876b191c98791571ca990f64a186edf67
Albert Andrejev <albert.andrejev@gmail.com>
2020-04-11 17:36:09 +0300

    New fetchMocks implementation

--- src/utils/fetchMock.ts
+++ src/utils/fetchMock.ts
@@ -1,31 +1,63 @@
-import fetchMock from "fetch-mock";
+/* eslint-disable no-global-assign */
+/* eslint-disable no-native-reassign */
 
 import { Consent } from "../store/consents";
+import sleep from "./test/sleep";
 
-const consents: Consent[] = [
-  {
-    name: "Bojack Horseman",
-    email: "bojack@horseman.com",
-    items: [
-      { title: "Receive newsletter", agree: true },
-      { title: "Be shown targeted ads", agree: true },
-    ],
-  },
-  {
-    name: "Princess Carolyn",
-    email: "princess@manager.com",
-    items: [{ title: "Receive newsletter", agree: true }],
-  },
+function createData(name: string, email: string, titles: string[]): Consent {
+  return { name, email, items: titles.map((title) => ({ title: title, agree: true })) };
+}
+
+const consents = [
+  createData("Bojack Horseman", "bojack@horseman.com", ["Receive newsletter", "Be shown targeted ads"]),
+  createData("Princess Carolyn", "princess@manager.com", ["Receive newsletter"]),
+  createData("User one", "one@user.com", ["Receive newsletter", "Contribute to anonymous usage statistics"]),
+  createData("User two", "two@user.com", ["Receive newsletter", "Contribute to anonymous usage statistics"]),
+  createData("User three", "three@user.com", [
+    "Be shown targeted ads",
+    "Contribute to anonymous usage statistics",
+  ]),
+  createData("User four", "four@user.com", [
+    "Be shown targeted ads",
+    "Receive newsletter",
+    "Contribute to anonymous usage statistics",
+  ]),
+  createData("User five", "five@user.com", ["Receive newsletter", "Be shown targeted ads"]),
+  createData("User six", "six@user.com", ["Receive newsletter", "Contribute to anonymous usage statistics"]),
 ];
 
-function setFetchMocks(): void {
-  fetchMock.get("/consents", consents, {
-    delay: 1000,
-  });
+const oldFetch = window.fetch;
 
-  fetchMock.post("/consents", 200, {
-    delay: 1000,
-  });
+async function getConsentsJson(): Promise<any> {
+  return consents;
+}
+
+async function fetchMock(input: RequestInfo, init?: RequestInit | undefined): Promise<Response> {
+  if (typeof input === "string" && input === "/consents") {
+    if (!init) {
+      await sleep(1000); // Add delay before returning answer, simulating network latency
+
+      // NB! If you do not need to use @ts-ignore anymore, then please remove "@typescript-eslint/ban-ts-ignore"
+      // rule from .eslintrc.js
+      // @ts-ignore
+      return {
+        ok: true,
+        json: getConsentsJson,
+      };
+    } else if (init.method === "POST" && init.body && typeof init.body === "string") {
+      consents.push(JSON.parse(init.body));
+      // @ts-ignore
+      return {
+        ok: true,
+      };
+    }
+  }
+
+  return oldFetch(input, init);
+}
+
+function setFetchMocks(): void {
+  window.fetch = fetchMock;
 }
 
 export default setFetchMocks;
